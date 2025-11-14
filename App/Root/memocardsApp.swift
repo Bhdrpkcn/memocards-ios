222663bb4c039a5d2400090dbbf887cdb4ab112e
Bhdrpkcn <bhdrpkcn@gmail.com>
2025-11-14 14:30:59 +0300

    first commit
    main structure

---
+++ App/Root/memocardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  memocardsApp.swift
+//  memocards
+//
+//  Created by Bahadır Pekcan on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct memocardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
